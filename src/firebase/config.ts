8b65ef1e416629ea25117e73d0c28493448e8299
Cynthia Griffin <cyn.tr6120234@gmail.com>
2026-03-01 07:09:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9405249848-4309d",
+  "appId": "1:14730905953:web:0cd57e0358ccea09afdd20",
+  "apiKey": "AIzaSyAHoz0DQlGT722dzOQFn5oWp1dm9N02mxw",
+  "authDomain": "studio-9405249848-4309d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "14730905953"
+};
